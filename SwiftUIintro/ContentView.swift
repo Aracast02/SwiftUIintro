099c5a53e56f55ec83ebe89a227b3fdf4a968124
Arantza Castro Dssavre <ary.castro.dessavre@gmail.com>
2024-09-03 10:05:40 -0600

    No funcionaba, ahora si (comentario de los cambios que se hicieron)

--- SwiftUIintro/ContentView.swift
+++ SwiftUIintro/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         TabView{
-            ContatsView()
+            ContactsView()
                 .tabItem {
                     Label("contacts", systemImage: "person.3.fill")
                 }
